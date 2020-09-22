4f90e37cf5d1f94f80d2181da6b9dca352bbfdfc
ibnHamdan <ibnHamdan@msn.com>
2020-09-22 04:30:15 +0300

    Updated add and delete post

--- app/components/Post.js
+++ app/components/Post.js
@@ -1,11 +1,17 @@
 import React, { useEffect } from "react"
 
 function Post(props) {
+  const deleteThePost = (e) => {
+    e.preventDefault()
+    props.deletePost(props.id)
+  }
   return (
     <li className="post">
       <div className="row">
         <div className="small-6 columns">
-          <h2 className="post-title">{props.subject}</h2>
+          <h2 className="post-title">
+            <a>{props.subject}</a>
+          </h2>
         </div>
         <div className="small-2 columns">
           <p>{props.author}</p>
@@ -14,7 +20,9 @@ function Post(props) {
           <p>rating</p>
         </div>
         <div className="small-2 columns">
-          <button className="button info expanded">edit</button>
+          <button onClick={deleteThePost} className="button info expanded">
+            delete
+          </button>
         </div>
       </div>
     </li>
